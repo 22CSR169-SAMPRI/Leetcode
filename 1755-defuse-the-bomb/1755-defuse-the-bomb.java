102d85fdb97ef5fa39d677e3aad397c5c8fddb1c
22CSR169-SAMPRI <166939061+22CSR169-SAMPRI@users.noreply.github.com>
2024-09-24 14:12:43 +0530

    Time: 2 ms (57.93%), Space: 42.3 MB (22.39%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -1,45 +1,29 @@
 class Solution {
-    public int[] decrypt(int[] code, int k) {
-
-        if(k == 0)
-        {
-            return new int[code.length];
-        }
-
-        int window = 0;
+     public int[] decrypt(int[] code, int k) {
         int[] ans = new int[code.length];
-
-        if(k > 0)
-        {
-            for(int i=1;i<=k;i++)
-            {
-                window += code[i % code.length];
-            }
-
-            for(int i=0;i<code.length;i++)
-            {
-                ans[i] = window;
-                window -= code[(i+1) % code.length];
-                window += code[(i + 1 + k) % code.length];
-            }
-        }
-        else if(k < 0)
-        {
-            for(int i=k;i<0;i++)
-            {
-                window += code[(code.length + i) % code.length];
-                // System.out.println("First : " + (code.length + i) % code.length);
-            }
-            // System.out.println("In loop : ");
-            for(int i=0;i<code.length;i++)
-            {
-                ans[i] = window;
-                window -= code[(code.length + i + k) % code.length];
-                window += code[i % code.length];
-                // System.out.println("Minus :" + (code.length + i + k) % code.length + " Plus : " + i % code.length);
-            }
+        if (k == 0) {
+            return ans;
         }
 
+        if (k > 0) {
+            for (int i = 0; i < code.length; i++) {
+                int sum = 0;
+                for (int j = 1; j <= k; j++) {
+                    sum += code[(i + j) % code.length];
+                }
+                ans[i] = sum;
+            }
+        }
+        else {
+            for (int i = 0; i < code.length; i++) {
+                int sum = 0;
+                for (int j = 1; j <= -1 * k; j++) {
+                    sum += code[(i - j + code.length) % code.length];
+
+                }
+                ans[i] = sum;
+            }
+        }
         return ans;
     }
 }
