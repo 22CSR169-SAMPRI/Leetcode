bcceae8d61aff09f30bcde00b36824e05e90ab2a
22CSR169-SAMPRI <166939061+22CSR169-SAMPRI@users.noreply.github.com>
2024-09-26 10:20:31 +0530

    Time: 7 ms (100%), Space: 49.3 MB (60.4%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -3,13 +3,24 @@ class NumArray {
 
     public NumArray(int[] nums) {
         this.nums=nums;
+        this.nums[0]=nums[0];
+        for(int i=1;i<nums.length;i++){
+            this.nums[i]=this.nums[i-1]+nums[i];
+        }
     }
     
     public int sumRange(int left, int right) {
+        /*
         int sum=0;
         for(int i=left;i<=right;i++)
         sum+=nums[i];
         return sum;
+        */
+
+        if(left==0)
+        return nums[right];
+        else
+        return nums[right]-nums[left-1];
     }
 }
 
