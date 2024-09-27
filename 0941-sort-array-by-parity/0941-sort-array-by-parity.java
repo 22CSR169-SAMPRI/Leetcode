bb0c2bd174e2f631a083e37f849c4a6d53cf566e
22CSR169-SAMPRI <166939061+22CSR169-SAMPRI@users.noreply.github.com>
2024-09-27 14:35:49 +0530

    Time: 0 ms (100%), Space: 44.5 MB (89.94%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
+        /*
         int i=0,j;
         
         for(i=0;i<nums.length-1;i++){
@@ -14,5 +15,28 @@ class Solution {
             }
         }
         return nums;
+        */
+        int i = 0;                  // Initialize the first pointer at the beginning of the array.
+        int j = nums.length - 1;    // Initialize the second pointer at the end of the array.
+
+        while (i < j) {
+            if (nums[i] % 2 == 0) {  // If nums[i] is even, increment i.
+                i++;
+            } else if (nums[j] % 2 != 0) {  // If nums[j] is odd, decrement j.
+                j--;
+            } else {  // If nums[i] is odd and nums[j] is even, swap them and update pointers.
+                swap(nums, i, j);
+                i++;
+                j--;
+            }
+        }
+
+        return nums;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
