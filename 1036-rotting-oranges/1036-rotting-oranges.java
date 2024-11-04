bf44294d69ad6e81fec6f20f5af747d915c78cc7
22CSR169-SAMPRI <166939061+22CSR169-SAMPRI@users.noreply.github.com>
2024-11-04 16:04:33 +0530

    Time: 3 ms (14.8%), Space: 42.4 MB (39.81%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,54 +1,52 @@
 import java.util.*;
-class orange{
-    int row,col,mins;
-    public orange(int r,int c,int m){
-        this.row = r;
-        this.col = c;
-        this.mins= m;
+class Pair{
+    int row,col,level;
+    Pair(int _row,int _col,int _level){
+        this.row=_row;
+        this.col=_col;
+        this.level=_level;
+
     }
 }
 class Solution {
     public int orangesRotting(int[][] grid) {
-         int r = grid.length;
-        int c = grid[0].length;
-        int fresh = 0;
-        Queue<orange> queue = new ArrayDeque<>();
-        for(int i= 0;i <r;i++){
-            for(int j=0;j<c;j++){
-               if(grid[i][j] == 1){
-                   fresh++;
-                   
-               } 
-               else if(grid[i][j] == 2){
-                   queue.add(new orange(i,j,0));
-                   grid[i][j] = 0;
-               }
+         Queue <Pair> q=new LinkedList<>();
+         int m=grid.length;
+         int n=grid[0].length;
+         int frsh=0;
+         int[][] vis=new int[m][n];
+         for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    q.offer(new Pair(i,j,0));
+                    vis[i][j]=1;
+                }
+                if(grid[i][j]==1)
+                frsh++;
             }
-        }
-        if(queue.isEmpty() && fresh == 0){
-            return 0;
-        }
-        int maxMins = 0;
-        int rowdiff[] = {-1,0,0,1};
-        int coldiff[] = {0,-1,1,0};
-        
-        while(!queue.isEmpty()){
-            orange curr = queue.poll();
-            maxMins = Math.max(maxMins,curr.mins);
+         }
+         int max_level=0;
+         int[] drow={0,1,0,-1};
+         int[] dcol={1,0,-1,0};
+         while(!q.isEmpty()){
+            int r=q.peek().row;
+            int c=q.peek().col;
+            int cur_level=q.peek().level;
+            q.poll();
+            max_level=Math.max(cur_level,max_level);
             for(int i=0;i<4;i++){
-                int adjr = curr.row+rowdiff[i];
-                int adjc = curr.col+coldiff[i];
-                if(adjr >= 0 && adjr<r && adjc>=0 && adjc<c && grid[adjr][adjc] == 1){
-                queue.add(new orange(adjr,adjc,curr.mins+1));
-                    
-                    fresh--;
-                grid[adjr][adjc] = 0;
-                } 
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                    q.offer(new Pair(nrow,ncol,cur_level+1));
+                    vis[nrow][ncol]=1;
+                    frsh--;
+                }
             }
-        }
-        if(fresh>0){
-            return -1;
-        }
-        return maxMins;
+
+         }
+         if(frsh!=0)
+         return -1;
+         return max_level;
         }
 }
